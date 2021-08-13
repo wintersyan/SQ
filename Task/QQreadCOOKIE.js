ae5fabd202a71845eda2ac3dfd5cc2bf855fbd8b
wintersyan <57772243+wintersyan@users.noreply.github.com>
2021-08-13 20:21:12 +0800

    Update QQreadCOOKIE.js

--- Task/QQreadCOOKIE.js
+++ Task/QQreadCOOKIE.js
@@ -1,5 +1,5 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
-let QQreadurlVal = ``
+let QQreadurlVal = `https://toblog.ctobsnssdk.com/service/2/app_log/?sdk_version=3303&version_code=7.6.60&language=zh&app_name=QQ%E9%98%85%E8%AF%BB&app_version=7.6.60&vid=1A66FF78-2959-4139-9C9D-CB398DFD985B&device_id=3281360957226180&channel=App%20Store&mcc_mnc=46001&host_appid=5112153&resolution=750*1334&aid=164362&os=iOS&openudid=f263e0e8f2371d2170bd4ee34184228cb5b3570c&timezone=8&ac=WIFI&device_model=iPhone10,1&os_version=14.7.1&device_platform=iphone&idfa=00000000-0000-0000-0000-000000000000`
 
 
 
