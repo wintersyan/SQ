70accb45bdf3e2f386d56514ae887d02e161596c
wintersyan <57772243+wintersyan@users.noreply.github.com>
2021-10-20 19:13:34 +0800

    Update

--- Task/txstock.js
+++ Task/txstock.js
@@ -989,7 +989,7 @@ async function userhome() {
         'Content-Type': `application/x-www-form-urlencoded`,
         'Referer': `http://zixuanguapp.finance.qq.com`,
         'Host': `wzq.tenpay.com`,
-        'User-Agent': `QQStock/8.7.1 (iPhone; iOS 14.1; Scale/2.00)`,
+        'User-Agent': `QQStock/8.8.1 (iPhone; iOS 14.3; Scale/2.00))`,
         'Accept-Encoding': `gzip,deflate`,
         'Accept-Language': `zh-Hans-CN;q=1`
       },
@@ -1001,7 +1001,9 @@ async function userhome() {
           console.log(JSON.stringify(err));
           $.logErr(err);
         } else {
+           //console.log(data);
           if (safeGet(data)) {
+
             money = JSON.parse(data);
             //console.log(money);
             $.log(`【账户金币】:${money.icon_amount}金币\n`);
