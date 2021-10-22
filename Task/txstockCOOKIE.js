bd2554f122fe7f1905f7069dc73edf54ecfab3f4
wintersyan <57772243+wintersyan@users.noreply.github.com>
2021-10-22 17:25:46 +0800

    Update txstockCOOKIE.js

--- Task/txstockCOOKIE.js
+++ Task/txstockCOOKIE.js
@@ -3,7 +3,7 @@ let userheaderVal= `oA0GbjvVrktMICKze0EFnSPvwo34&_appName=ios&_dev=iPhone10,1&_d
 oA0GbjlR_YMYMMtVeeF_vqNV9EkE&_appName=ios&_dev=iPhone10,1&_devId=82795e016e290d124bee883a0e96cb549ba8364b&_appver=9.5.0&_ifChId=&_isChId=1&_osVer=15.0.2&openid=oA0GbjlR_YMYMMtVeeF_vqNV9EkE&fskey=v0aaf8a8222617268a418c7e05e95e1e&appid=wxcbc3ab3807acb685&access_token=50_L5vnaRz2rIA_6Yjp0YMTW6Onl2xk9gehHwpWUwkS-IaMyjTiWRZ1KL3dApG5C7DdWJMwezwxxYafz4BGK5GQvXL9ZK1dDai2tTjVHpFsWKE&buildType=store&check=11&_idfa=&lang=zh_CN`
 
 let userkeyVal= `pgv_pvid=1400747250
-pgv_pvid=9557182382; ts_uid=6694562050`
+pgv_pvid=9557182382`
 
 let cashheaderVal= `5
 5`
@@ -14,7 +14,7 @@ v0aaf8a8222617268a418c7e05e95e1e&channel=1&access_token=50_L5vnaRz2rIA_6Yjp0YMTW
 let signkeyVal= `{""}`
 
 let taskheaderVal= `&_dev=iPhone10,1&_devId=82795e016e290d124bee883a0e96cb549ba8364b&_appver=9.4.1&_ifChId=&_isChId=1&_osVer=15.0.2&openid=oA0GbjvVrktMICKze0EFnSPvwo34&fskey=v0aaf63c2216159bd2ca9d03e95e1cf7&appid=wxcbc3ab3807acb685&access_token=50_jnwuSVvCld542AooqE0k-Th3BBG3gUZU6Z71vthZlieE-sCQvkdEQXEp-rxM9Y5NrpWEML2o7w9J5ROBHHo4NkUbGoW44ZKGMKZhHNNhH1c&buildType=store&check=11&_idfa=&lang=zh_CN
-_dev=iPhone10,1&_devId=82795e016e290d124bee883a0e96cb549ba8364b&_appver=9.5.0&_ifChId=&_isChId=1&_osVer=15.0.2&openid=oA0GbjlR_YMYMMtVeeF_vqNV9EkE&fskey=v0aaf8a8222617268a418c7e05e95e1e&appid=wxcbc3ab3807acb685&access_token=50_L5vnaRz2rIA_6Yjp0YMTW6Onl2xk9gehHwpWUwkS-IaMyjTiWRZ1KL3dApG5C7DdWJMwezwxxYafz4BGK5GQvXL9ZK1dDai2tTjVHpFsWKE&buildType=store&check=11&_idfa=&lang=zh_CN`
+&_dev=iPhone10,1&_devId=82795e016e290d124bee883a0e96cb549ba8364b&_appver=9.5.0&_ifChId=&_isChId=1&_osVer=15.0.2&openid=oA0GbjlR_YMYMMtVeeF_vqNV9EkE&fskey=v0aaf8a8222617268a418c7e05e95e1e&appid=wxcbc3ab3807acb685&access_token=50_L5vnaRz2rIA_6Yjp0YMTW6Onl2xk9gehHwpWUwkS-IaMyjTiWRZ1KL3dApG5C7DdWJMwezwxxYafz4BGK5GQvXL9ZK1dDai2tTjVHpFsWKE&buildType=store&check=11&_idfa=&lang=zh_CN`
 
 let taskkeyVal= `pgv_info=ssid=s7539094912; pgv_pvid=1400747250; wzq_channel=fm_wzq_wx_v1_unknow_01..
 pgv_pvid=9557182382; ts_uid=5876449217; ts_refer=zqact03.tenpay.com/`
